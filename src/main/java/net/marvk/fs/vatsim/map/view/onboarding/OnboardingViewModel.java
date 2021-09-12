9ff6809c060698dcb4a54ca37680a01e51048ec1
Marvin <marvk3@gmail.com>
2021-09-12 03:46:35 +0200

    Added theme choice endpoint request in onboarding

--- src/main/java/net/marvk/fs/vatsim/map/view/onboarding/OnboardingViewModel.java
+++ src/main/java/net/marvk/fs/vatsim/map/view/onboarding/OnboardingViewModel.java
@@ -1,16 +1,36 @@
 package net.marvk.fs.vatsim.map.view.onboarding;
 
+import com.google.inject.Inject;
 import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.ReadOnlyBooleanWrapper;
+import lombok.extern.log4j.Log4j2;
+import net.marvk.fs.vatsim.map.api.VatprismApi;
+import net.marvk.fs.vatsim.map.api.VatprismApiException;
 import net.marvk.fs.vatsim.map.view.Notifications;
 
+@Log4j2
 public class OnboardingViewModel implements ViewModel {
     private final ReadOnlyBooleanWrapper onboardingComplete = new ReadOnlyBooleanWrapper();
+    private final VatprismApi vatprismApi;
+
+    @Inject
+    public OnboardingViewModel(final VatprismApi vatprismApi) {
+        this.vatprismApi = vatprismApi;
+    }
 
     public void setTheme(final Theme theme) {
         Notifications.SET_THEME.publish("VATprism " + theme.getThemeName());
-        skip();
+        trySubmitThemeChoiceApiRequest(theme);
+        onboardingComplete.set(true);
+    }
+
+    private void trySubmitThemeChoiceApiRequest(final Theme theme) {
+        try {
+            vatprismApi.submitThemeChoice(theme.getThemeName());
+        } catch (final VatprismApiException e) {
+            log.error("Failed to send request to theme choice endpoint", e);
+        }
     }
 
     public void skip() {
