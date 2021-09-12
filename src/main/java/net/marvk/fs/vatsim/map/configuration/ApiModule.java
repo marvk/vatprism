8d4a447e697e9016f24ab3082c2f0e3d96dc83bc
Marvin <marvk3@gmail.com>
2021-09-12 04:09:47 +0200

    Fixed ssl error

--- src/main/java/net/marvk/fs/vatsim/map/configuration/ApiModule.java
+++ src/main/java/net/marvk/fs/vatsim/map/configuration/ApiModule.java
@@ -13,9 +13,9 @@ public class ApiModule extends AbstractModule {
     protected void configure() {
         bind(VatprismApi.class).to(HttpVatprismApi.class).in(Singleton.class);
         bind(String.class).annotatedWith(Names.named("apiVersionUrl"))
-                          .toInstance("https://version.vatprism.org:6300/versionRequest?version=%s&channel=%s");
+                          .toInstance("http://version.vatprism.org:6300/versionRequest?version=%s&channel=%s");
         bind(String.class).annotatedWith(Names.named("apiThemeUrl"))
-                          .toInstance("https://version.vatprism.org:6300/themeChoice?version=%s&theme=%s");
+                          .toInstance("http://version.vatprism.org:6300/themeChoice?version=%s&theme=%s");
         bind(Duration.class).annotatedWith(Names.named("versionApiTimeout")).toInstance(Duration.ofMillis(2500));
     }
 }
